e1061d866862ee7f0c09e981090b2170ca56b7cb
Andre0n <andreon098@gmail.com>
2022-02-10 15:25:37 -0300

    :sparkles: Add enemy.js

---
+++ modules/enemy.js
@@ -0,0 +1,23 @@
+import createCircle from "./circle.js";
+import vector2 from "./vector2.js";
+
+const ENEMY_RADIUS = 30;
+const ENEMY_SPEED = 300;
+const ENEMY_COLOR = "#FF0"
+
+const createEnemy = (position) => {
+    const enemy = createCircle(ENEMY_RADIUS, ENEMY_SPEED, 
+                                ENEMY_COLOR, position);
+    enemy.isAlive = true;
+    enemy.velocity = vector2();
+    enemy.update = (delta, targetPosition) => {
+        let direction = enemy.position.angleToPoint(targetPosition);
+        enemy.velocity = vector2(Math.cos(direction), Math.sin(direction));
+        enemy.velocity = (enemy.velocity.scalar(enemy.speed)).scalar(delta);
+        enemy.position = enemy.position.add(enemy.velocity);
+    };
+    enemy.draw = context => enemy.render(context);
+    return enemy;
+};
+
+export default createEnemy;
