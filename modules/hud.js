7ffc67efad6d2852f6272555dd6cd1e6acbb6904
Andre0n <andreon098@gmail.com>
2022-02-11 11:51:15 -0300

    :bug: Update position of text in hud.js.

--- modules/hud.js
+++ modules/hud.js
@@ -1,11 +1,14 @@
+import vector2  from "./vector2.js";
 import createText from "./text.js";
 
-const hud =()=>{
+const hud = (() =>{
     return {
         draw(context, playerHealth){
             let playerHealthText = createText(`Health: ${playerHealth}`,
-                                               "#ffdd59", 0, vector2(0, 0));
+                                               "#ffdd59", 0, vector2(30, 80));
             playerHealthText.render(context);
         }
     }
-};
+})();
+
+export default hud;
