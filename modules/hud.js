e51b0e3b8efa366e45136408b131d452a7470bd4
Andre0n <andreon098@gmail.com>
2022-02-11 12:16:25 -0300

    :sparkles: Add playe score text in hud.js

--- modules/hud.js
+++ modules/hud.js
@@ -3,10 +3,13 @@ import createText from "./text.js";
 
 const hud = (() =>{
     return {
-        draw(context, playerHealth){
+        draw(context, playerHealth, playerScore){
             let playerHealthText = createText(`Health: ${playerHealth}`,
                                                "#ffdd59", 0, vector2(30, 80));
+            let playerScoreText = createText(`Health: ${playerScore}`,
+                                               "#ffdd59", 1, vector2(30, 100));
             playerHealthText.render(context);
+            playerScoreText.render(context);
         }
     }
 })();
