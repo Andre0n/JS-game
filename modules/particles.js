583f0b01ea212dd81a414a3761c8f5eed38b5cd1
Andre0n <andreon098@gmail.com>
2022-02-16 18:58:47 -0300

    :sparkles: Add particle.js module.

---
+++ modules/particles.js
@@ -0,0 +1,22 @@
+import createCircle from "./circle.js";
+import utils from "./utils.js";
+
+const PARTICLE_RADIUS = 5;
+const PARTICLE_SPEED = 500;
+const PARTICLE_MAX_HEALTH = 10;
+const PARTICLE_LIFE_TIME = 1;
+
+const createParticle = (position, color) => {
+    const particle = createCircle(PARTICLE_RADIUS, PARTICLE_SPEED, 
+                                    color, 0, position);
+    particle.lifeTime = PARTICLE_LIFE_TIME;
+    particle.velocity = vector2(randomInRange(-PARTICLE_SPEED, PARTICLE_SPEED),
+                              randomInRange(-PARTICLE_SPEED, PARTICLE_SPEED);
+    particle.update = delta => {
+        particle.lifeTime -= delta;
+        particle.position = particle.position.add(particle.velocity.scalar(delta));
+    };
+    particle.draw = context => particle.render(context);
+
+    return particle;
+};
