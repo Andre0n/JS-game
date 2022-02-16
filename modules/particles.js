998941d4cfc1db2278a46f404110deda1b5f4f86
Andre0n <andreon098@gmail.com>
2022-02-16 20:24:53 -0300

    :bug: Fixing bug in particles.js

--- modules/particles.js
+++ modules/particles.js
@@ -1,4 +1,5 @@
 import createCircle from "./circle.js";
+import vector2 from "./vector2.js";
 import utils from "./utils.js";
 
 const PARTICLE_RADIUS = 5;
@@ -10,13 +11,15 @@ const createParticle = (position, color) => {
     const particle = createCircle(PARTICLE_RADIUS, PARTICLE_SPEED, 
                                     color, 0, position);
     particle.lifeTime = PARTICLE_LIFE_TIME;
-    particle.velocity = vector2(randomInRange(-PARTICLE_SPEED, PARTICLE_SPEED),
-                              randomInRange(-PARTICLE_SPEED, PARTICLE_SPEED);
+    particle.velocity = 
+        vector2(utils.randomInRange(-PARTICLE_SPEED, PARTICLE_SPEED), 
+                utils.randomInRange(-PARTICLE_SPEED, PARTICLE_SPEED));
+
     particle.update = delta => {
         particle.lifeTime -= delta;
         particle.position = particle.position.add(particle.velocity.scalar(delta));
     };
     particle.draw = context => particle.render(context);
-
     return particle;
 };
+export default createParticle;
